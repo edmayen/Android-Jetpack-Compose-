7a286c3b0c974dc84636bae5d7198bd18aa3913f
Eduardo Mayen <ed_mayen@hotmail.com>
2022-06-21 19:24:14 -0500

    Creating an Image Card Composable

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -6,68 +6,85 @@ import android.text.Layout
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var context: Context
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        context = this
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Column(
-                        modifier = Modifier
-                            .background(Color.Green)
-                            .fillMaxWidth()
-                            .fillMaxHeight(0.50f)
-//                            .requiredWidth(300.dp)
-                            .border(5.dp, Color.Magenta)
-                            .padding(5.dp)
-                            .border(5.dp, Color.Blue)
-                            .padding(5.dp)
-                            .border(10.dp, Color.Red)
-                            .padding(10.dp)
-                    ) {
-                        Greeting("Eduardo")
-                        Spacer(modifier = Modifier.height(50.dp))
-                        Greeting(name = "Android")
-                    }
+            val painter = painterResource(id = R.drawable.tmnt)
+            val description =  "Teenage Mutant Ninja Turtles"
+            val title = "Teenage Mutant Ninja Turtles"
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
 
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier
-        .clickable {
-
-        })
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter, 
+                contentDescription = contentDescription, 
+                contentScale = ContentScale.Inside
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart) {
+                Text(title, style = TextStyle(color = Color.White), fontSize = 16.sp)
+            }
+        }
     }
 }
